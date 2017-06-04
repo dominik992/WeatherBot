46a7640c5eb7ddd27556585134ce9480e0b9fcb2
dominik992 <dominik.dragicevic.fer@gmail.com>
2017-06-04 14:58:03 +0200

    rm api docs from build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import { watherRouter } from './weather';
 import { messengerRouter } from './messenger';
 export const router = new express.Router();
 
-router.use('/v1/weather/', watherRouter);
 router.use('/v1/messenger-hooks/', messengerRouter);
